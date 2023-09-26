825d97bb1050c9165595fabf428a99c8831da629
Jennifer-monteiro <70246891+Jennifer-monteiro@users.noreply.github.com>
2023-09-26 06:43:24 -0400

    changes

--- property.py
+++ property.py
@@ -1,29 +1,37 @@
-
 class Colors:
     RESET = "\033[0m"
     GREEN = "\033[32m"
     RED = "\033[31m"
     BLUE = "\033[34m"
 
-def calculate_roi(property_price, monthly_rental_income, annual_expenses):
-    annual_income = monthly_rental_income * 12
-    annual_profit = annual_income - annual_expenses
-    roi = (annual_profit / property_price) * 100
-    return roi
+class ROICalculator:
+    def __init__(self):
+        self.property_price = 0
+        self.monthly_rental_income = 0
+        self.annual_expenses = 0
 
-def main():
-    property_price = float(input(f"{Colors.BLUE}Enter the property price: {Colors.RESET}"))
-    monthly_rental_income = float(input(f"{Colors.BLUE}Enter the monthly rental income: {Colors.RESET}"))
-    annual_expenses = float(input(f"{Colors.BLUE}Enter the annual property expenses: {Colors.RESET}"))
+    def gather_user_input(self):
+        self.property_price = float(input(f"{Colors.BLUE}Enter the property price: {Colors.RESET}"))
+        self.monthly_rental_income = float(input(f"{Colors.BLUE}Enter the monthly rental income: {Colors.RESET}"))
+        self.annual_expenses = float(input(f"{Colors.BLUE}Enter the annual property expenses: {Colors.RESET}"))
 
-    roi = calculate_roi(property_price, monthly_rental_income, annual_expenses)
+    def calculate_roi(self):
+        annual_income = self.monthly_rental_income * 12
+        annual_profit = annual_income - self.annual_expenses
+        roi = (annual_profit / self.property_price) * 100
+        return roi
 
-    if roi >= 0:
-        roi_text = f"{Colors.GREEN}{roi:.2f}%{Colors.RESET}"
-    else:
-        roi_text = f"{Colors.RED}{roi:.2f}%{Colors.RESET}"
+    def run(self):
+        self.gather_user_input()
+        roi = self.calculate_roi()
 
-    print(f"The Return on Investment (ROI) for the property is: {roi_text}")
+        if roi >= 0:
+            roi_text = f"{Colors.GREEN}{roi:.2f}%{Colors.RESET}"
+        else:
+            roi_text = f"{Colors.RED}{roi:.2f}%{Colors.RESET}"
+
+        print(f"The Return on Investment (ROI) for the property is: {roi_text}")
 
 if __name__ == "__main__":
-    main()
+    roi_calculator = ROICalculator()
+    roi_calculator.run()
